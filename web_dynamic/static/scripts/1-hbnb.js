a1502642a8c7b2bdeee6f86804bbc783d80d354a
Robert Odhiambo <odhizrober254@gmail.com>
2024-02-09 19:10:52 +0300

    implement: 1-hbnb.html and 1-hbnb.py to render ammenities with a checkbox to update the selectedAmenities with JQuery to render the template

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,16 @@
-/* global $ */
-document.addEventListener('DOMContentLoaded', () => {
-  
-});
+$(document).ready(function () {
+  const selectedAmenities = {};
+
+  $('input[type="checkbox"]').change(function (data) {
+    const amenity_id = $(this).data('id');
+    const amenity_name = $(this).data('name');
+
+    if (this.checked) {
+      selectedAmenities[amenity_id] = amenity_name;
+    } else {
+      delete selectedAmenities[amenity_id];
+    }
+
+    $('h4').text(Object.values(selectedAmenities).join(', '));
+  });
+})
