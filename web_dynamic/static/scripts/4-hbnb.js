ebacb9435c5065b0ef4451a82075f2fef8ca8ae1
Robert Odhiambo <odhizrober254@gmail.com>
2024-02-09 23:02:04 +0300

    implemented: Filtering places by amenities

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  const selectedAmenities = {};
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+  function loadPlaces(amenities) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: JSON.stringify({amenities}),
+    success: function(data) {
+      $('section.places').empty();
+      for (const place of data) {
+        const article = `
+	  <article>
+              <div class="title_box">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">
+	              ${place.price_by_night}
+		  </div>
+              </div>
+              <div class="information">
+                  <div class="max_guest">
+		      ${place.max_guest} Guests
+		  </div>
+                  <div class="number_rooms">
+		      ${place.number_rooms} Bedrooms
+		  </div>
+                  <div class="number_bathrooms">
+		      ${place.number_bathrooms} Bathrooms
+		  </div>
+	     </div>
+	     <div class="description">${place.description}</div>
+         </article>`;
+	    $('section.places').append(article);
+	}
+    }
+  });
+  };
+	
+  $('input[type="checkbox"]').change(function (data) {
+    const amenity_id = $(this).data('id');
+    const amenity_name = $(this).data('name');
+
+    if (this.checked) {
+      selectedAmenities[amenity_id] = amenity_name;
+    } else {
+      delete selectedAmenities[amenity_id];
+    }
+
+    $('h4').text(Object.values(selectedAmenities).join(', '));
+  });
+
+  $('button').click(function () {
+    const selectedAmenitiesList = Object.values(selectedAmenities);
+    loadPlaces(selectedAmenitiesList);
+  }
+});
