9cbdf89ade2877e8a56aa138d475e9d0d6433c5f
Robert Odhiambo <odhizrober254@gmail.com>
2024-02-09 22:15:39 +0300

    updated 2-hbnb.html to: check the api-status and a javascript file to change the class based on the status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  const selectedAmenities = {};
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+  $('input[type="checkbox"]').change(function (data) {
+    const amenity_id = $(this).data('id');
+    const amenity_name = $(this).data('name');
+
+    if (this.checked) {
+      selectedAmenities[amenity_id] = amenity_name;
+    } else {
+      delete selectedAmenities[amenity_id];
+    }
+
+    $('h4').text(Object.values(selectedAmenities).join(', '));
+  });
+})
