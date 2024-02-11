7de2ecb992c168a1aeca995a33416c9b626186d5
Brian <chumobrian@gmail.com>
2024-02-11 23:49:16 +0300

    Fix(2-hbnb.js): update jQuery selector for amenities checkboxes to specifically target checkboxes within the amenities section

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,6 +18,6 @@ $(document).ready(function () {
       delete selectedAmenities[amenity_id];
     }
 
-    $('h4').text(Object.values(selectedAmenities).join(', '));
+    $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
   });
 })
